d03beca3cbea516815a67275aab18f06dca29587
Surfpeny420 <surfpeny@gmail.com>
2025-10-25 17:13:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,46 @@
-const CACHE_NAME = 'crypto-miner-v1';
+const CACHE_NAME = 'crypto-miner-max-v3';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/app.js'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
